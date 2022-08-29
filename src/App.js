54617720bc9f48848b3fca4d476a3628f54f9773
Christoph Könekamp <Christoph.koenekamp@icloud.com>
2022-08-29 17:11:33 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,28 +1,20 @@
-import logo from './logo.png';
 import './App.css';
-import True from './fontTrue.css'
+import True from './fontTrue.css';
+import logo from './logo.png';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p style={{ fontFamily: "True" }}>
-          Welcome to Nerve Global
-        </p>
-        <a style={{ fontSize: "20px" }}>
-          If you are not redirected automatically,
-        </a>
-        <a
-          style={{ fontSize: "30px" }}
-          className="App-link"
-          href="https://www.nerveglobal.com"
-        >
-          click here
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p style={{ fontFamily: 'True' }}>Welcome to Nerve Global</p>
+				<a style={{ fontSize: '20px' }}>If you are not redirected automatically,</a>
+				<a style={{ fontSize: '30px' }} className="App-link" href="https://www.nerveglobal.com">
+					click here
+				</a>
+			</header>
+		</div>
+	);
 }
 
 export default App;
