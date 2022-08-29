7da0aab7c453d900d7348f93c9ef7064599c1b71
Christoph Könekamp <Christoph.koenekamp@icloud.com>
2022-08-29 17:39:44 +0100

    update

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
 	return (
 		<div className="App">
 			<header className="App-header">
+				<meta property="og:image" content="https://app.nerveglobal.com/logo192.png" />
 				<img src={logo} className="App-logo" alt="logo" />
 				<p style={{ fontFamily: 'True' }}>Welcome to Nerve Global</p>
 				<a style={{ fontSize: '20px' }}>If you are not redirected automatically,</a>
